223a5adc6defe1054ce689a2f52a5e552a7b5f27
umrichen <richen@umich.edu>
2016-01-24 23:54:25 -0500

    completed

--- list.c
+++ list.c
@@ -10,7 +10,21 @@
 list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
-
+	list_t *temp = head;
+	if (new_element -> index <= temp -> index) {
+		new_element -> next = head;
+		head = new_element;
+	}
+	else {
+		while (new_element -> index > temp -> index) {
+			if (temp -> next != NULL && (new_element -> index > temp -> next -> index)) temp = temp -> next;
+			else {
+				new_element -> next = temp -> next;
+				temp -> next = new_element;
+				break;
+			}
+		}
+	}
 	return head;
 }
 
@@ -18,7 +32,18 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
-
+	if (head -> next != NULL) {
+		list_t* previous = head;
+		list_t* current = previous -> next;
+		list_t* next = current -> next;
+		previous -> next = NULL;
+		while (current != NULL) {
+			current -> next = previous;
+			previous = current;
+			current = next;
+			if (next != NULL) next = next -> next;
+		}
+		head = previous;
+	}
 	return head;
 }
-
